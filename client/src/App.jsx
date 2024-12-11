a65d03a5d05d3bebf71305b1a25567022dd4791f
mrinnnmoy <mrinmoy202000@gmail.com>
2024-12-11 20:00:07 +0530

    setup-WEB3_walletIntegration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./App.css";
 
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <ConnectButton />
+    </div>
   )
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
+import '@rainbow-me/rainbowkit/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiProvider } from 'wagmi';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { config } from './wagmi';
+// import { mainnet, polygon, optimism, arbitrum, base, } from 'wagmi/chains';
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>,
 )
